fd384277ead15fc03eaa8782864fbd145f8b7518
gasdar <gottafindshape@gmail.com>
2023-12-19 15:23:41 -0300

    Podcast-FM 1.0: Termino de proyecto, agregando apartados estadisticas, planes, testimoniales, footer

--- Podcast-FM/gulpfile.js
+++ Podcast-FM/gulpfile.js
@@ -16,8 +16,7 @@ function css( done ) {
     src('src/scss/app.scss')
         .pipe( sourcemaps.init() )
         .pipe( sass() )
-        // .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
         .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css') )
 
